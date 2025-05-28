66e8be0fd536b5c2758b43199b39e7d68671c907
dgrtp <daungrouprtp@gmail.com>
2025-05-28 15:03:40 +0700

    Update and rename Copyright to Copyright.js

--- Copyright.js
+++ Copyright.js
@@ -1,5 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script>
+
+
   $(document).ready(function () {
     // Fungsi saat teks dicopy
     document.oncopy = function () {
@@ -52,4 +52,4 @@
       }
     });
   });
-</script>
+
