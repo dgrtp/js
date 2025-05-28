3d330c8955c9b29b05badd2e0943f489a8bda417
dgrtp <daungrouprtp@gmail.com>
2025-05-28 15:26:38 +0700

    Update crdaunnews.js

--- crdaunnews.js
+++ crdaunnews.js
@@ -1,32 +1,23 @@
 $(document).ready(function () {
     // Fungsi saat teks dicopy
     document.oncopy = function () {
-      var t = $(".jdl_dtl").text(),
-        e = document.location.href,
-        a = $("#editor").text(),
-        i = $("#penulis").text(),
-        n = "",
-        o = "";
-      a && (n = "<br />" + a);
-      i && (o = "  |" + i);
-      var r,
-        l = n.concat(o),
-        d = document.getElementsByTagName("body")[0],
-        p =
-          "Dilarang keras menyalin, memodifikasi, produksi ulang, menerbitkan ulang, upload ulang, serta mendistribusikan ulang semua konten Business Insight dalam bentuk apa pun tanpa izin tertulis terlebih dahulu dari Kontan. Semua konten dalam Business Insight adalah hak milik Kontan dan dilindungi oleh UU Hak Cipta.<br /><br />DaunNews<br />www.dgjackpot.site",
-        r = window.getSelection(),
-        s = document.createElement("div");
-      s.style.position = "absolute";
-      s.style.left = "-99999px";
-      d.appendChild(s);
-      s.innerHTML = p;
-      r.selectAllChildren(s);
-      window.setTimeout(function () {
-        d.removeChild(s);
+      var tambahanInfo = "Dilarang keras menyalin, memodifikasi, produksi ulang, menerbitkan ulang, upload ulang, serta mendistribusikan ulang semua konten Business Insight tanpa izin tertulis terlebih dahulu dari Kontan. Semua konten dalam Business Insight adalah hak milik Kontan dan dilindungi oleh UU Hak Cipta.<br /><br />DaunNews<br />www.dgjackpot.site";
+
+      var body = document.getElementsByTagName("body")[0];
+      var selection = window.getSelection();
+      var div = document.createElement("div");
+      div.style.position = "absolute";
+      div.style.left = "-99999px";
+      body.appendChild(div);
+      div.innerHTML = tambahanInfo;
+      selection.selectAllChildren(div);
+
+      setTimeout(function () {
+        body.removeChild(div);
       }, 0);
     };
 
-    // Blokir F12, Ctrl+U, dan Ctrl+Shift+I lalu redirect
+    // Blokir F12, Ctrl+U, Ctrl+Shift+I lalu redirect
     document.addEventListener("keydown", function (e) {
       if (
         e.key === "F12" || e.keyCode === 123 ||
